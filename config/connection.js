315ba8e142cf5f2cdda263a7ec37a8a1257ba57f
hossam2671 <108755474+hossam2671@users.noreply.github.com>
2024-04-06 22:32:35 +0200

    ....

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { MONGODB_URI } = process.env;
 
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, {
+  connectTimeoutMS: 30000, // 30 seconds
+  socketTimeoutMS: 45000, // 45 seconds
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log('Connected to MongoDB');
   })
